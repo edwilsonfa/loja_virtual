65f699d2afd6ce0b288405793621152917ff8c50
edwilson <edwilsonfa@gmail.com>
2023-12-19 17:40:58 -0300

    Criada a entidade de Acesso

---
+++ src/main/java/com/learning/loja_virtual/model/Acesso.java
@@ -0,0 +1,58 @@
+package com.learning.loja_virtual.model;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "acesso")
+@SequenceGenerator(name = "seq_acesso", sequenceName = "seq_acesso", initialValue = 1, allocationSize = 1)
+public class Acesso implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_acesso")
+    private Long id;
+    @Column(nullable = false)
+    private String descricao;
+    @Override
+    public String getAuthority() {
+        return null;
+    }
+
+    public Acesso() {
+    }
+
+    public Acesso(Long id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Acesso acesso = (Acesso) o;
+        return Objects.equals(id, acesso.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
